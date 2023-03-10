2e2f398cb4135c4a441bf8b77fc296311113d9b4
Zeynep Özdemir <zeynepozz2299@gmail.com>
2023-03-10 20:30:56 +0300

    It finds also reverse of sentence not only words and checks it for palindrome word or sentence.

--- PalindromeWord.java
+++ PalindromeWord.java
@@ -5,27 +5,16 @@ import java.util.Scanner;
 public class PalindromeWord {
 
 	public static void main(String[] args) {
-		String word, reverseWord;
-		Scanner scanner = new Scanner(System.in);
 
-		System.out.print("Enter the word: ");
-		word = scanner.next();
-		scanner.close();
+		System.out.print("Enter the word/sentence: ");
+		String word = getWord();
 
-		reverseWord = reverseWord(word);
-		System.out.println("\nReverse of the \"" + word + "\" is " + reverseWord + "\n");
-
-		if (ispalindrome(word, reverseWord))
-			System.out.println("\"" + word + "\" is a palindrome word.");
-
-		else
-			System.out.println("\"" + word + "\" is not a palindrome word.");
-
+		printReverseWord(word);
+		printIsPandrome(word);
 	}
 
 	public static boolean ispalindrome(String word, String reverseWord) {
 		return (word.equals(reverseWord));
-
 	}
 
 	public static String reverseWord(String word) {
@@ -34,8 +23,35 @@ public class PalindromeWord {
 		for (int i = word.length() - 1; i >= 0; i--) {
 			reverseWord += word.charAt(i);
 		}
-
 		return reverseWord;
+	}
+
+	public static String upperCase(String word) {
+		return word.toUpperCase();
+	}
+
+	public static String removeSpace(String word) {
+		return word.replaceAll("\\s", "");
+	}
+
+	public static String getWord() {
+		Scanner scanner = new Scanner(System.in);
+		return scanner.nextLine();
+	}
+
+	public static void printReverseWord(String word) {
+		String reverseWord = reverseWord(word);
+		System.out.println("\nReverse of the \n" + word + " --> " + reverseWord + " \n");
+	}
+
+	public static void printIsPandrome(String word) {
+		String reverseWord = reverseWord(word);
+
+		if (ispalindrome(removeSpace(upperCase(word)), removeSpace(upperCase(reverseWord))))
+			System.out.println("\" " + word + " \" is a palindrome word.");
+
+		else
+			System.out.println("\" " + word + " \" is not a palindrome word.");
 	}
 
 }
